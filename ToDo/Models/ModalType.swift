b498607a70fa5c173a07900b45468969f8d3bf93
filmore28 <58660891+filmore28@users.noreply.github.com>
2022-01-12 17:16:05 -0500

    modal type added

--- ToDo/Models/ModalType.swift
+++ ToDo/Models/ModalType.swift
@@ -5,4 +5,24 @@
 //  Created by Philip Moore on 2022-01-12.
 //
 
-import Foundation
+import SwiftUI
+
+enum ModalType: Identifiable, View {
+    case new
+    case update(ToDo)
+    var id: String {
+        switch self {
+        case .new: return "new"
+        case .update: return "update"
+        }
+    }
+    
+    var body: some View {
+        switch self {
+        case .new:
+            return ToDoFormView(formVM: ToDoFormViewModel())
+        case .update(let toDo):
+            return ToDoFormView(formVM: ToDoFormViewModel(toDo))
+        }
+    }
+}
