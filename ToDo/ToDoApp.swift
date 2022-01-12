378f28af16ea4aacf472cc156dc154096a43c633
filmore28 <58660891+filmore28@users.noreply.github.com>
2022-01-12 16:08:22 -0500

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Philip Moore on 2022-01-12.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
